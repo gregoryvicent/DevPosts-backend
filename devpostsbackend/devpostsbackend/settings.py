9abbb9d454423da5f90e49ba742aa48801915cc0
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-10-20 15:00:53 -0400

    Created 'managedevposts' app

--- devpostsbackend/devpostsbackend/settings.py
+++ devpostsbackend/devpostsbackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'manageposts.apps.ManagepostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
